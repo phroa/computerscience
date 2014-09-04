b2b3316b8367b58d83c44c1b402bc0fbd2ba384e
Phroa <nr.phroa@gmail.com>
2014-09-03 19:52:34 -0700

    Day 1

---
+++ day_1/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] argv) {
+        System.out.println("Hello, world");
+        System.out.println("This program produces");
+        System.out.println();
+        System.out.println("four lines of output.");
+    }
+}
