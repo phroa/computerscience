6bf69e90100bebc6f46ca1cecb6a3a4bc44190fa
Phroa <nr.phroa@gmail.com>
2014-09-20 11:50:42 -0700

    Homework for monday

---
+++ homework/Ex 2-5/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by js on 9/20/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i=0;i<=5;i++) {
+            for (int j=0;j<i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
