088ea9d517df988aeba8f4d43267fe6ecbfcd537
Phroa <nr.phroa@gmail.com>
2014-09-25 09:07:00 -0700

    Day 11

---
+++ day_11/Project 2-2/src/Main.java
@@ -0,0 +1,31 @@
+// Jack & Sam
+
+public class Main {
+
+    public static void main(String[] args) {
+        line();
+
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("|  ^^  |");
+            System.out.println("| ^  ^ |");
+            System.out.println("|^    ^|");
+        }
+
+
+        line();
+
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("|v    v|");
+            System.out.println("| v  v |");
+            System.out.println("|  vv  |");
+        }
+
+        line();
+
+    }
+
+    static void line() {
+        System.out.println("+------+");
+    }
+
+}
