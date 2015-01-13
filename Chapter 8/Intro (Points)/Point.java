55d4fa297c0aff521bfcdfbd51d5fda4d3343630
Phro a <nr.phroa@gmail.com>
2015-01-13 08:59:35 -0800

    Encapsulate Point

--- Chapter 8/Intro (Points)/Point.java
+++ Chapter 8/Intro (Points)/Point.java
@@ -11,6 +11,22 @@ public final class Point {
     this.y = y;
   }
 
+  public int getX() {
+    return this.x;
+  }
+
+  public int getY() {
+    return this.y;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
   public void setLocation(int x, int y) {
     this.x = x;
     this.y = y;
--- Chapter 8/Intro (Points)/PointMain.java
+++ Chapter 8/Intro (Points)/PointMain.java
@@ -1,15 +1,15 @@
 public class PointMain {
   public static void main(String[] args) {
     Point one = new Point();
-    one.x = 30;
-    one.y = 40;
+    one.setX(30);
+    one.setY(40);
 
     System.out.print("Point one is at ");
     System.out.println(one);
 
     Point two = new Point();
-    two.x = 35;
-    two.y = 48;
+    two.setX(35);
+    two.setY(48);
 
     System.out.print("Point two is at ");
     System.out.println(two);
